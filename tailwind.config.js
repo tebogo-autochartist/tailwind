3070a86ee315e7ddd00db6e6defcff1d43024d22
tebogo-autochartist <tebogo@autochartist.com>
2024-07-04 16:38:15 +0200

    complete first row

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "regal-blue": "#243c5a",
+      },
+    },
   },
   plugins: [],
 };
